e9a6bddaa3d001966ac6f84e551f2d138a89ed79
Yao Shao <yshao998@gmail.com>
2019-07-07 12:04:40 +0800

    add test code

---
+++ Calculator/Test/Test.cpp
@@ -0,0 +1,23 @@
+﻿#include "../Code/Calculator.h"
+#include "../Code/Calculator.cpp"
+#include <iostream>
+
+int main()
+{
+	Calculator c;
+	int a, b;
+	a = 1;
+	b = 2;
+
+	if (c.add(a, b) != (a + b)) {
+		std::cout << "Add Error!" << std::endl;
+		return -1;
+	}
+	if (c.sub(a, b) != (a - b)) {
+		std::cout << "Sub Eroor!" << std::endl;
+		return -2;
+	}
+
+
+	return 0;
+}
