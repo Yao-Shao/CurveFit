a3593ad9e16c67688fc1b7a9fed37cb3977d7bdb
Yao Shao <yshao998@gmail.com>
2019-07-07 14:04:33 +0800

    add bug

--- Calculator/Code/Calculator.cpp
+++ Calculator/Code/Calculator.cpp
@@ -8,7 +8,7 @@ int Calculator::add(int num1, int num2)
 
 int Calculator::sub(int num1, int num2)
 {
-	return num1 - num2;  // 错误，应该是 num1 - num2
+	return num2 - num2;  // 错误，应该是 num1 - num2
 }
 
 Calculator::Calculator()
