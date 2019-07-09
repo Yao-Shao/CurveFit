19095ed8e86c6a1e10d9f172ceac67ae07a48d1c
ZZH <2467882606@qq.com>
2019-07-09 09:04:42 +0800

    modified model

--- OPCF/MODEL/Model.cpp
+++ OPCF/MODEL/Model.cpp
@@ -3,9 +3,35 @@
 
 Model::Model()
 {
+	sp_Fuction = std::make_shared<Function>();
 }
 
 std::shared_ptr<Function> Model::getFunction()
 {
 	return std::shared_ptr<Function>();
+}
+
+void Model::opcf_createFunction(Type t)
+{
+	if (t == LINEAR_FUNCTION)
+	{
+
+	}
+	else if (t == QUADRATIC_FUNCTION) {
+
+	}
+	else if(t == EXPONENTIAL_FUNCTION){
+
+	}
+	else if (t == LN_FUNCTION) {
+
+	}
+	else if (t == NORMAL_FUNCTION) {
+	
+	}
+	else {
+
+	}
+	//告知其它模块，model里面的Function已经改变
+	Fire_OnPropertyChanged("Fuction");
 }
